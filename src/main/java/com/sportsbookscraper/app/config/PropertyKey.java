d490c61ded798aaa41f9d5c22f5b424de9921c9c
jhenly <JonathanHenly@gmail.com>
2020-11-18 03:29:55 -0500

    Add default values to enum initialization

--- src/main/java/com/sportsbookscraper/app/config/PropertyKey.java
+++ src/main/java/com/sportsbookscraper/app/config/PropertyKey.java
@@ -1,7 +1,7 @@
 package com.sportsbookscraper.app.config;
 
 /**
- * Enums representing the keys in a properties file.
+ * Enum representing the keys in a properties or preference file.
  *
  * @author Jonathan Henly
  */
@@ -9,25 +9,25 @@ package com.sportsbookscraper.app.config;
 enum PropertyKey {
 
     /* - keys of properties shared by all sheets - */
-    EXCEL_FILE_PATH("excel.file.path"),
-    ALL_SHEETS("all.sheets"),
-    SHEET_FONT(ALL_SHEETS + ".font"),
-    SHEET_FONT_SIZE(SHEET_FONT + ".size"),
-    COLS_SIZETOFIT(ALL_SHEETS + ".cols.sizetofit"),
-    ROWS_SIZETOFIT(ALL_SHEETS + ".rows.sizetofit"),
+    EXCEL_FILE_PATH("excel.file.path"), // no default
+    ALL_SHEETS("all.sheets"), // no default, throw exception if not found
+    SHEET_FONT(ALL_SHEETS + ".font", "Calibri"),
+    SHEET_FONT_SIZE(SHEET_FONT + ".size", 11),
+    COLS_SIZETOFIT(ALL_SHEETS + ".cols.sizetofit", true),
+    ROWS_SIZETOFIT(ALL_SHEETS + ".rows.sizetofit", false),
 
     /* - individual sheet property keys - */
-    SCRAPE_URL(".scrape.url"),
-    SHEET_TITLE(".sheet.title"),
-    TITLE_ROW(SHEET_TITLE + ".row"),
-    TITLE_COL(SHEET_TITLE + ".row"),
-    SHEET_TABLE(".sheet.table"),
-    TEAMS_COL(SHEET_TABLE + ".teams.col"),
-    OPENER(SHEET_TABLE + ".opener"),
-    OPENER_COL(SHEET_TABLE + ".opener.col"),
-    BOOKIE_COL(SHEET_TABLE + ".bookie.col");
+    SCRAPE_URL(".scrape.url"), // no default, throw exception if not found
+    SHEET_TITLE(".sheet.title"), // no default
+    TITLE_ROW(SHEET_TITLE + ".row", 1),
+    TITLE_COL(SHEET_TITLE + ".col", 0),
+    SHEET_TABLE(".sheet.table", 1),
+    TEAMS_COL(SHEET_TABLE + ".teams.col", 0),
+    OPENER(SHEET_TABLE + ".opener", true),
+    OPENER_COL(SHEET_TABLE + ".opener.col", 1),
+    BOOKIE_COL(SHEET_TABLE + ".bookie.col", 2);
 
-
+    /* enum members */
     private String key; // the key in the properties file
     private String def; // the key's string default
     private int idef; // the key's int default
@@ -51,7 +51,7 @@ enum PropertyKey {
     String key() { return key; }
     
     /** @return this property's string default, if it has one */
-    String getsdef() {
+    String sdef() {
         return def;
     }
     
