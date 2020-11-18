806126f179a8c254ba76e5020c8043af04164940
jhenly <JonathanHenly@gmail.com>
2020-11-18 03:40:02 -0500

    Handle exceptions and provide building blocks for sheet editing

--- src/main/java/com/sportsbookscraper/app/excel/ExcelFileWriter.java
+++ src/main/java/com/sportsbookscraper/app/excel/ExcelFileWriter.java
@@ -1,10 +1,35 @@
 package com.sportsbookscraper.app.excel;
 
-public class ExcelFileWriter {
+import java.io.IOException;
 
-	
-	ExcelFileWriter(String excelFilePath) {
-		
-	}
-	
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+
+/**
+ * Wrapper class for Apache POI's {@link XSSFWorkbook} class.
+ * <p>
+ * The main purpose of this class is to prevent the accidental altering of an
+ * Excel file. This class also includes multiple helper functions.
+ *
+ * @author Jonathan Henly
+ */
+public class ExcelFileWriter {
+    /* private members */
+    private final Workbook workbook;
+
+    /* opens an Excel workbook from the passed in file path */
+    ExcelFileWriter(String excelFilePath) throws IOException {
+        this(excelFilePath, false);
+    }
+    
+    ExcelFileWriter(String excelFilePath, boolean createNew)
+    throws IOException
+    {
+        workbook = new XSSFWorkbook(excelFilePath);
+    }
+    
+    
+    // Use the following to create a hyperlink in Excel via Apache POI
+    // wb.getCreationHelper().createHyperlink(HyperlinkType.URL).;
 }
