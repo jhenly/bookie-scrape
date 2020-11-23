6badfc0277ea067c636445bd25586663f48ab505
Jonathan Henly <JonathanHenly@gmail.com>
2020-11-23 16:52:38 -0500

    Use LF instead of CRLF

--- src/main/java/com/sportsbookscraper/app/sample/Main.java
+++ src/main/java/com/sportsbookscraper/app/sample/Main.java
@@ -13,13 +13,11 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
         primaryStage.setTitle("TNSA Odds Scrapper");
-        primaryStage.setScene(new Scene(root, 480, 132));
+        primaryStage.setScene(new Scene(root, 500, 600));
         primaryStage.show();
     }
     
     
-    public static void main(String[] args) {
-        launch(args);
-    }
-
+    public static void main(String[] args) { launch(args); }
+    
 }
