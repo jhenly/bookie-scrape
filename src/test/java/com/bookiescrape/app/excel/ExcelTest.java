7e6b0a783509d4f405d028590a36bee016b2cfe9
jhenly <JonathanHenly@gmail.com>
2020-12-01 16:13:27 -0500

    Update Excel tests

--- src/test/java/com/bookiescrape/app/excel/ExcelTest.java
+++ src/test/java/com/bookiescrape/app/excel/ExcelTest.java
@@ -6,33 +6,33 @@ import java.io.IOException;
 
 import org.junit.Test;
 
-import com.bookiescrape.app.excel.WorkbookFactory;
-import com.bookiescrape.app.excel.WorkbookReader;
 
 /**
  * Unit test for simple App.
  */
 public class ExcelTest {
+
     /**
      * Rigorous Test :-)
      */
     @Test
     public void shouldAnswerWithTrue() { assertTrue(true); }
-    
+
     @Test(expected = org.apache.poi.openxml4j.exceptions.InvalidOperationException.class)
-    public void callingExcelFactoryNewExcelFileReaderWithWrongPathShouldThrowIOException() {
+    public void
+    callingExcelFactoryNewExcelFileReaderWithWrongPathShouldThrowIOException()
+    {
         boolean didThrow = false;
         WorkbookReader efr = null;
         try {
             efr = WorkbookFactory.newWorkbookReader("/tmp");
-            
+
             efr.close();
         } catch (IOException ioe) {
             didThrow = true;
         }
-        
+
         assertTrue(didThrow);
     }
-    
-    
+
 }
