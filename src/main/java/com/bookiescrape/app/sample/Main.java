12e154a8ae8e3beb1dffd6a2fc4b5256dbdcef55
jhenly <JonathanHenly@gmail.com>
2020-12-01 23:05:12 -0500

    Remove title bar from main window

--- src/main/java/com/bookiescrape/app/sample/Main.java
+++ src/main/java/com/bookiescrape/app/sample/Main.java
@@ -5,19 +5,20 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 
 public class Main extends Application {
-    
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
-        primaryStage.setTitle("Bookie Scrape");
+        primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.setScene(new Scene(root, 500, 600));
         primaryStage.show();
     }
-    
-    
+
+
     public static void main(String[] args) { launch(args); }
-    
+
 }
