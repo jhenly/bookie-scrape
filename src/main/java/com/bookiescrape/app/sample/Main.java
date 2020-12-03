3e7ec5cc7b4280c0ccfe4cbe3f6ab3c80748f5e0
jhenly <JonathanHenly@gmail.com>
2020-12-02 19:47:41 -0500

    Fix main.fxml resource path
    - Updated Main's documentation.

--- src/main/java/com/bookiescrape/app/sample/Main.java
+++ src/main/java/com/bookiescrape/app/sample/Main.java
@@ -1,5 +1,7 @@
 package com.bookiescrape.app.sample;
 
+import java.net.URL;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,17 +10,43 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 
+/**
+ * Main application class.
+ * <p>
+ * This class creates and launches the JavaFX application.
+ *
+ * @author Jonathan Henly
+ */
 public class Main extends Application {
+
+    private static final String MAIN_FXML = "/fxml/main.fxml";
     
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
+        URL mainFxmlUrl = getClass().getResource(MAIN_FXML);
+        
+        // load the main fxml file
+        Parent root = FXMLLoader.load(mainFxmlUrl);
+
+        // create window with no title bar or default min, max, close buttons
         primaryStage.initStyle(StageStyle.UNDECORATED);
+
+        // create and set the main scene
         primaryStage.setScene(new Scene(root, 500, 600));
+
+        // display the window
         primaryStage.show();
     }
-    
-    
-    public static void main(String[] args) { launch(args); }
-    
+
+
+    /**
+     * Entry point of the application.
+     *
+     * @param args - command line arguments
+     */
+    public static void main(String[] args) {
+        // launch the JavaFX application
+        launch(args);
+    }
+
 }
