61496286ef9b017a0638376ebfea81b0cba4c3a5
jhenly <JonathanHenly@gmail.com>
2021-03-29 16:18:49 -0400

    Use try-with-resources when creating Excel workbook

--- src/main/java/com/bookiescrape/app/excel/WorkbookFactory.java
+++ src/main/java/com/bookiescrape/app/excel/WorkbookFactory.java
@@ -33,9 +33,7 @@ public final class WorkbookFactory {
      * @throws IOException if an I/O error occurs
      * @throws FileNotFoundException if the specified file path does not exist
      */
-    public static WorkbookReader newWorkbookReader(String workbookFilePath)
-        throws IOException, FileNotFoundException
-    {
+    public static WorkbookReader newWorkbookReader(String workbookFilePath) throws IOException, FileNotFoundException {
         return new WorkbookReader(workbookFilePath);
     }
     
@@ -46,9 +44,7 @@ public final class WorkbookFactory {
      * @return a new Excel workbook writer instance
      * @throws IOException if an I/O error occurs
      */
-    public static WorkbookWriter newWorkbookWriter(String workbookFilePath)
-        throws IOException
-    {
+    public static WorkbookWriter newWorkbookWriter(String workbookFilePath) throws IOException {
         return new WorkbookWriter(workbookFilePath);
     }
     
@@ -61,9 +57,7 @@ public final class WorkbookFactory {
      *         Excel workbook
      * @throws IOException if an I/O error occurs
      */
-    public static WorkbookWriter
-    createNewWorkbookFile(String newWorkbookFilePath) throws IOException
-    {
+    public static WorkbookWriter createNewWorkbookFile(String newWorkbookFilePath) throws IOException {
         doCreateWorkbookChecks(newWorkbookFilePath); // throws IOException
         createWriteAndCloseNewWorkbookFile(newWorkbookFilePath);
         
@@ -71,14 +65,11 @@ public final class WorkbookFactory {
     }
     
     /* checks new Excel file path and throws exceptions accordingly */
-    private static String doCreateWorkbookChecks(String newExcelFilePath)
-        throws IOException
-    {
+    private static String doCreateWorkbookChecks(String newExcelFilePath) throws IOException {
         Objects.requireNonNull(newExcelFilePath);
         
         if (newExcelFilePath.isBlank()) {
-            throw new IllegalArgumentException(
-                "cannot create a new Excel file with an empty string");
+            throw new IllegalArgumentException("cannot create a new Excel file with an empty string");
         }
         
         Path newFilePath = (new File(newExcelFilePath)).toPath().normalize();
@@ -91,29 +82,22 @@ public final class WorkbookFactory {
         if (!Files.isDirectory(newFilePath.getParent().normalize())) {
             /* new file's parent is not a directory, so throw */
             throw new IOException(String.format(
-                "cannot create new Excel file, the parent directory in '%s' is"
-                    + "not a directory",
-                newExcelFilePath));
+                "cannot create new Excel file, the parent directory in '%s' is" + "not a directory", newExcelFilePath));
         }
         
         return newExcelFilePath;
     }
-
+    
     /* function that creates, writes and closes a new Excel workbook file */
-    private static void
-    createWriteAndCloseNewWorkbookFile(String newExcelFilePath)
-        throws IOException
-    {
+    private static void createWriteAndCloseNewWorkbookFile(String newExcelFilePath) throws IOException {
         // create new workbook and file output stream
-        Workbook wb = XSSFWorkbookFactory.createWorkbook(OPCPackage.create(newExcelFilePath));
-        FileOutputStream out = new FileOutputStream(newExcelFilePath);
-
-        // write new workbook to file
-        wb.write(out);
-
-        // close file output stream and workbook
-        out.close();
-        wb.close();
+        try (OPCPackage opcPackage = OPCPackage.create(newExcelFilePath);
+        Workbook wb = XSSFWorkbookFactory.createWorkbook(opcPackage);
+        FileOutputStream out = new FileOutputStream(newExcelFilePath);)
+        {
+            // write new workbook to file
+            wb.write(out);
+        }
     }
-
+    
 }
